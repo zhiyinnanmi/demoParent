0258076a291d47356d3466a3f509451dbf25ed81
wangzhy <643380288@qq.com>
2019-03-31 23:27:47 +0800

    2019-03-31

---
+++ demo-provider-dept-8001/src/main/java/com/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,30 @@
+package com.springcloud.service.impl;
+
+import com.springcloud.dao.DeptDao;
+import com.springcloud.entity.Dept;
+import com.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptDao dao;
+
+    @Override
+    public boolean add(Dept dept) {
+        return dao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list() {
+        return dao.findAll();
+    }
+}
